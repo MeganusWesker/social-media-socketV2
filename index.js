74e6107caa559496a866a33799a7d229a1aee2cd
MeganusWesker <mohitjassal157@gmail.com>
2023-03-27 16:07:03 +0530

    v2

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const {Server} =require('socket.io');
 
 module.exports.io=new Server(server,{
     cors:{
-        origin:'*',
-        
+        origin:'http://localhost:3000',
+        credentials:true,
     }
 });
 
